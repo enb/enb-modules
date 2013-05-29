310bf931c86df9d8a1e278be94b45516132aeb71
Marat Dulin <mdevils@yandex-team.ru>
2013-05-29 16:03:13 +0400

    Version 0.0.6

--- techs/deps-with-modules.js
+++ techs/deps-with-modules.js
@@ -33,7 +33,8 @@ var Vow = require('vow'),
     vm = require('vm'),
     vowFs = require('vow-fs'),
     DepsResolver = require('enb/lib/deps/deps-resolver'),
-    inherit = require('inherit');
+    inherit = require('inherit'),
+    modules = require('../lib/modules');
 
 module.exports = inherit(require('enb/lib/tech/base-tech'), {
 
@@ -147,33 +148,14 @@ var ModulesDepsResolver = inherit(DepsResolver, {
         });
         files.forEach(function(file) {
             var fileContent = fs.readFileSync(file.fullname, 'utf-8');
-            var modulesDefinition = fileContent.match(/modules\.define\([\s\n\r]*['"][^"']+["'][\s\n\r]*,[\s\n\r]*\[([^\]]+)\]/);
-            if (modulesDefinition) {
-                var depList = modulesDefinition[1].split(',').map(function(s) { return s.trim().replace(/^['"]|['"]$/g, ''); });
-                depList.forEach(function(dep) {
-                    var decl = {}, modParts;
-                    if (~dep.indexOf('__')) {
-                        var blockElemParts = dep.split('__');
-                        decl.name = blockElemParts[0];
-                        var elemParts = blockElemParts[1].split('_');
-                        decl.elem = elemParts[0];
-                        modParts = elemParts.slice(1);
-                    } else {
-                        var blockParts = dep.split('_');
-                        decl.name = blockParts[0];
-                        modParts = blockParts.slice(1);
-                    }
-                    if (modParts.length) {
-                        decl.modName = modParts[0];
-                        decl.modVal = modParts[1] || '';
-                    }
-                    var key = declKey(decl);
-                    if (!shouldDepsIndex[key]) {
-                        shouldDepsIndex[key] = true;
-                        result.shouldDeps.push(decl);
-                    }
-                });
-            }
+            var extractedDeps = modules.extractDependencies(fileContent);
+            extractedDeps.forEach(function(decl) {
+                var key = declKey(decl);
+                if (!shouldDepsIndex[key]) {
+                    shouldDepsIndex[key] = true;
+                    result.shouldDeps.push(decl);
+                }
+            });
         });
         return result;
     }
