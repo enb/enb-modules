ea0b5cd49602760d9ddcbd7d50b068206543f184
Anton Winogradov <winogradovaa@gmail.com>
2016-05-18 18:20:55 +0300

    check by typeof

--- techs/prepend-modules.js
+++ techs/prepend-modules.js
@@ -38,7 +38,7 @@ module.exports = require('enb/lib/build-flow').create()
     .builder(function(preTargetSource) {
         return vowFs.read(this._modulesFile, 'utf8').then(function(modulesRes) {
             return modulesRes +
-                "if(window === 'undefined' && typeof module !== 'undefined') {" +
+                "if(typeof window === 'undefined' && typeof module !== 'undefined') {" +
                 "modules = module.exports;" +
                 "}\n" +
                 preTargetSource;
